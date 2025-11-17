c65f3fbdba97421ea474b83379f42299b00da6b2
3la2i <alaa.ata25@gmail.com>
2025-11-17 18:17:44 +0300

    ss

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-app.use('/api/product', require('./Routes/product'));
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
