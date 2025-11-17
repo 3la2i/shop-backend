759f80c7121349c8cff36ff711ce2595adbedaec
3la2i <alaa.ata25@gmail.com>
2025-11-17 16:33:33 +0300

    puysing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
-
+app.use('/api/product', require('./Routes/product'));
 // Middlewares
 app.use(cors());
 app.use(express.json());
